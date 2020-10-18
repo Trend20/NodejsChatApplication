2fe8a49921b012b8dbefb2650aebb3072852efd4
devenock <enockomondi305@gmail.com>
2020-10-18 13:53:19 -0400

    adding socket.io

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const bodyParser = require('body-parser');
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
 
@@ -14,4 +21,17 @@ app.get('/messages', (req, res) => {
     res.send(messages);
 })
 
-app.listen(3000);
+// POST
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+});
+
+const server = http.listen(3000, function() {
+    console.log('Server is Listening on port', server.address().port);
+});
