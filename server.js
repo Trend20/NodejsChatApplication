12cf6799de1fe67fd4369aad8428b2fe9f2317ad
devenock <enockomondi305@gmail.com>
2020-10-18 12:49:13 -0400

    Adding routes

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ const app = express();
 
 app.use(express.static(__dirname))
 
+// Routes
+
+const messages = [
+        { name: 'Tim', message: 'Hi' },
+        { name: 'Jane', message: 'Hello' }
+    ]
+    // GET
+app.get('/messages', (req, res) => {
+    res.send(messages);
+})
+
 app.listen(3000);
